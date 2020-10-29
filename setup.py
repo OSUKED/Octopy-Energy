522dfb706bf418c07f57b2e57ac1e1a4456c023a
AyrtonB <AyrtonBourn@Outlook.com>
2020-10-29 11:31:42 +0000

    fixed docs deployment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="octopyenergy",
-    version="0.0.3",
+    version="0.0.4",
     author="Ayrton Bourn",
     author_email="ayrtonbourn@outlook.com",
     description="Python client for the Octopus energy API",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AyrtonB/Octopy-Energy", 
     packages=setuptools.find_packages(),
+    package_data={'octopyenergy': ['end_points.json']},
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
